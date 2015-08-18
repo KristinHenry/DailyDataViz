28daccb4b82f68cabedb65ecd10248802f665ccd
KristinHenry <galaxygoo@gmail.com>
2015-08-18 00:00:46 -0700

    updating today's viz

---
+++ 08_17_2015/data_csv.js
@@ -0,0 +1,17 @@
+
+// console.log(" data csv loading script");
+
+// var my_data = function()
+// {
+//     d3.csv("SSA-SA-FYWL.csv", function(prices){
+    
+//         console.log("prices: ")
+//         data = prices.map(function(d)
+//         {
+//             //each d is one line of the csv file represented as a json object
+//             console.log("d", d)
+//             return {"data": 'test'}
+//         })
+
+//     })
+// }
