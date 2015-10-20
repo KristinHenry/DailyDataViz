a34870a4b2907ad9d3198ac34b30e0cea4f678d8
KristinHenry <galaxygoo@gmail.com>
2015-10-20 14:44:00 -0700

    renaming folders

---
+++ sourceFiles/2015_08_29/README.md
@@ -0,0 +1,10 @@
+# August 29, 2015
+
+SSA Disability Claims 
+
+data source: Social Security Administration
+(http://catalog.data.gov/dataset/ssa-disability-claim-data)
+
+Switching Gears, and using p5.js (based on processing).
+
+
---
+++ sourceFiles/2015_08_29/sketch.js
@@ -0,0 +1,87 @@
+
+var table;
+var blobs = [];
+
+function preload(){
+    table = loadTable("SSA-SA-FYWL_modified.csv", "header");
+}
+
+function setup() {
+  // uncomment this line to make the canvas the full size of the window
+  //createCanvas(windowWidth, windowHeight);
+  var myCanvas = createCanvas(800, 500);
+  myCanvas.parent('p5exp');
+
+
+  console.log(table);
+
+    print(table.getColumnCount());
+    print(table.columns);
+
+
+    print(table.getColumn("State_Code"));
+    for(var i=0; i<table.getRowCount(); i++){
+    //for(var i=0; i<20; i++){
+        var d = table.getRow(i).obj;
+
+        print(d);
+        print(d["State_Code"])
+        print(d["Region_Code"])
+        print(d["Date"])
+        print('Population_age_18-64: ' + d['Population_age_18-64']);
+        print('Population_under_age_18: ' + d['Population_under_age_18']);
+        print('Percent_of_Population_under_age_18_Receiving_SSI_DC_Benefits: ' + d['Percent_of_Population_under_age_18_Receiving_SSI_DC_Benefits']);
+    
+        var kids = d['Population_under_age_18'];
+        // var adults = d['Population_age_18-64'];
+        // var state = d['State-Code'];
+        // var region = d['Region_Code'];
+        // var x = i*10;
+        // var y = i*4;
+
+        var x = i+10;
+        var y = 500; //250; //i;
+
+        var b = new Blob(x,y, kids);
+        b.display();
+
+        blobs.push(b);
+
+    }
+}
+
+function draw() {
+    // this is the timeline loop! 
+  // draw stuff here
+  //ellipse(width/2, height/2, 50, 50);
+  
+}
+
+//--------------------------------------------------
+
+
+Blob.prototype.display = function(){
+  //   stroke(0);
+  //   strokeWeight(2);
+  //   fill(255,127);
+  //   ellipse(this.position.x, this.position.y, this.kids/100, this.kids/100);
+
+  // }
+
+}
+
+
+
+function Blob(x, y, kids){
+    this.position = createVector(x,y);
+    this.kids = kids;
+}
+
+Blob.prototype.display = function(){
+    fill(255, 127);
+    var w = 2; //12; //random(10,100);
+    var h = Math.floor(this.kids/10000); // 20; //random(10,100);
+    ellipse(this.position.x, this.position.y, w, h);
+}
+
+ 
